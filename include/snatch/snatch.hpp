05f7350de79e04e362f1d8d536ba8bee89b8e046
Corentin Schreiber <github@corentin.net>
2022-10-30 10:13:49 +0000

    Disable do/while placeholder in pragma macros

--- include/snatch/snatch.hpp
+++ include/snatch/snatch.hpp
@@ -1008,22 +1008,22 @@ struct with_what_contains : private contains_substring {
 #    define SNATCH_WARNING_PUSH _Pragma("clang diagnostic push")
 #    define SNATCH_WARNING_POP _Pragma("clang diagnostic pop")
 #    define SNATCH_WARNING_DISABLE_PARENTHESES _Pragma("clang diagnostic ignored \"-Wparentheses\"")
-#    define SNATCH_WARNING_DISABLE_CONSTANT_COMPARISON do {} while (0)
+#    define SNATCH_WARNING_DISABLE_CONSTANT_COMPARISON
 #elif defined(__GNUC__)
 #    define SNATCH_WARNING_PUSH _Pragma("GCC diagnostic push")
 #    define SNATCH_WARNING_POP _Pragma("GCC diagnostic pop")
 #    define SNATCH_WARNING_DISABLE_PARENTHESES _Pragma("GCC diagnostic ignored \"-Wparentheses\"")
-#    define SNATCH_WARNING_DISABLE_CONSTANT_COMPARISON do {} while (0)
+#    define SNATCH_WARNING_DISABLE_CONSTANT_COMPARISON
 #elif defined(_MSC_VER)
 #    define SNATCH_WARNING_PUSH _Pragma("warning(push)")
 #    define SNATCH_WARNING_POP _Pragma("warning(pop)")
-#    define SNATCH_WARNING_DISABLE_PARENTHESES do {} while (0)
+#    define SNATCH_WARNING_DISABLE_PARENTHESES
 #    define SNATCH_WARNING_DISABLE_CONSTANT_COMPARISON _Pragma("warning(disable: 4127)")
 #else
-#    define SNATCH_WARNING_PUSH do {} while (0)
-#    define SNATCH_WARNING_POP do {} while (0)
-#    define SNATCH_WARNING_DISABLE_PARENTHESES do {} while (0)
-#    define SNATCH_WARNING_DISABLE_CONSTANT_COMPARISON do {} while (0)
+#    define SNATCH_WARNING_PUSH
+#    define SNATCH_WARNING_POP
+#    define SNATCH_WARNING_DISABLE_PARENTHESES
+#    define SNATCH_WARNING_DISABLE_CONSTANT_COMPARISON
 #endif
 // clang-format on
 
