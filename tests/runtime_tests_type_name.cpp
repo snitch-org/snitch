f4740a4ee69c32fb239fde891299267f9d8735c8
Corentin Schreiber <github@corentin.net>
2022-11-02 22:12:51 +0000

    Use matchers::is_any_of in type_name test

--- tests/runtime_tests_type_name.cpp
+++ tests/runtime_tests_type_name.cpp
@@ -1,5 +1,7 @@
 #include "testing.hpp"
 
+using namespace std::literals;
+
 struct global_test_struct {
     int  i = 0;
     bool b = true;
@@ -13,12 +15,24 @@ TEST_CASE("type name", "[utility]") {
 
     CHECK(snatch::type_name<int> == "int");
     CHECK(snatch::type_name<unsigned> == "unsigned int");
-    CHECK(snatch::type_name<long> == "long int");
+    CHECK(snatch::type_name<long> == snatch::matchers::is_any_of("long int"sv, "long"sv));
     CHECK(snatch::type_name<float> == "float");
     CHECK(snatch::type_name<double> == "double");
-    CHECK(snatch::type_name<void*> == "void*");
-    CHECK(snatch::type_name<const void*> == "const void*");
-    CHECK(snatch::type_name<std::string_view> == "std::basic_string_view<char>");
-    CHECK(snatch::type_name<global_test_struct> == "global_test_struct");
+    CHECK(snatch::type_name<void*> == snatch::matchers::is_any_of("void*"sv, "void *"sv));
+
+    CHECK(
+        snatch::type_name<const void*> ==
+        snatch::matchers::is_any_of("const void*"sv, "const void *"sv));
+
+    CHECK(
+        snatch::type_name<std::string_view> ==
+        snatch::matchers::is_any_of(
+            "std::basic_string_view<char>"sv, "std::string_view"sv,
+            "class std::basic_string_view<char,struct std::char_traits<char> >"sv));
+
+    CHECK(
+        snatch::type_name<global_test_struct> ==
+        snatch::matchers::is_any_of("global_test_struct"sv, "struct global_test_struct"sv));
+
     CHECK(snatch::type_name<test_struct>.ends_with("test_struct"));
 };
