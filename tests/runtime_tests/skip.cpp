d1db51bb922909e44fc0983f196d9e43e452a8c4
Corentin Schreiber <github@corentin.net>
2022-11-20 09:07:02 +0000

    Added more skip tests

--- tests/runtime_tests/skip.cpp
+++ tests/runtime_tests/skip.cpp
@@ -29,6 +29,32 @@ TEST_CASE("skip", "[test macros]") {
         framework.run_test();
         CHECK(framework.get_num_skips() == 1u);
     }
+
+    SECTION("skip failure") {
+        framework.test_case.func = [](snatch::impl::test_run& SNATCH_CURRENT_TEST) {
+            SNATCH_SKIP("hello");
+            SNATCH_FAIL_CHECK("trigger");
+        };
+
+        framework.run_test();
+        CHECK(framework.get_num_skips() == 1u);
+        CHECK(framework.get_num_failures() == 0u);
+    }
+
+    SECTION("skip section") {
+        framework.test_case.func = [](snatch::impl::test_run& SNATCH_CURRENT_TEST) {
+            SNATCH_SECTION("section 1") {
+                SNATCH_SKIP("hello");
+            }
+            SNATCH_SECTION("section 2") {
+                SNATCH_FAIL_CHECK("trigger");
+            }
+        };
+
+        framework.run_test();
+        CHECK(framework.get_num_skips() == 1u);
+        CHECK(framework.get_num_failures() == 0u);
+    }
 };
 
 SNATCH_WARNING_POP
