7b85ba1decf420bce3a7cf3a4c7a073ab03d4e02
Corentin Schreiber <github@corentin.net>
2023-03-26 10:02:19 +0100

    Remove unnecessary include to <compare>

--- include/snitch/snitch.hpp
+++ include/snitch/snitch.hpp
@@ -9,7 +9,6 @@
 #    include <exception> // for std::exception
 #endif
 #include <bit> // for compile-time float to string
-#include <compare> // for std::partial_ordering
 #include <initializer_list> // for std::initializer_list
 #include <limits> // for compile-time integer to string
 #include <optional> // for cli
