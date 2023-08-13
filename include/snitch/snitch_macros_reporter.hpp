fc756ebe9c134fdea4474ff1e4c18d6f1628c79b
Corentin Schreiber <github@corentin.net>
2023-08-13 16:59:42 +0100

    Removed extra semicolon

--- include/snitch/snitch_macros_reporter.hpp
+++ include/snitch/snitch_macros_reporter.hpp
@@ -7,7 +7,7 @@
 
 #define SNITCH_REGISTER_REPORTER_CALLBACKS(NAME, ...)                                              \
     static const std::string_view SNITCH_MACRO_CONCAT(reporter_id_, __COUNTER__)                   \
-        [[maybe_unused]] = snitch::tests.add_reporter(NAME, __VA_ARGS__);
+        [[maybe_unused]] = snitch::tests.add_reporter(NAME, __VA_ARGS__)
 
 #define SNITCH_REGISTER_REPORTER_IMPL(NAME, TYPE, COUNTER)                                         \
     static std::optional<TYPE> SNITCH_MACRO_CONCAT(reporter_, COUNTER);                            \
@@ -31,7 +31,7 @@
             NAME, &SNITCH_MACRO_CONCAT(reporter_init_, COUNTER),                                   \
             &SNITCH_MACRO_CONCAT(reporter_config_, COUNTER) < TYPE >,                              \
             &SNITCH_MACRO_CONCAT(reporter_report_, COUNTER),                                       \
-            &SNITCH_MACRO_CONCAT(reporter_finish_, COUNTER));
+            &SNITCH_MACRO_CONCAT(reporter_finish_, COUNTER))
 
 #define SNITCH_REGISTER_REPORTER(NAME, TYPE) SNITCH_REGISTER_REPORTER_IMPL(NAME, TYPE, __COUNTER__)
 
