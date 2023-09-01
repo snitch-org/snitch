b0e6eb06815dc7d3b07053c17555be326c55896c
Corentin Schreiber <github@corentin.net>
2023-09-01 09:50:17 +0100

    MSVC wants [[noreturn]] leftmost

--- include/snitch/snitch_error_handling.hpp
+++ include/snitch/snitch_error_handling.hpp
@@ -10,11 +10,11 @@ namespace snitch {
 // Maximum length of error messages.
 constexpr std::size_t max_message_length = SNITCH_MAX_MESSAGE_LENGTH;
 
-SNITCH_EXPORT [[noreturn]] void terminate_with(std::string_view msg) noexcept;
+[[noreturn]] SNITCH_EXPORT void terminate_with(std::string_view msg) noexcept;
 
 SNITCH_EXPORT extern small_function<void(std::string_view)> assertion_failed_handler;
 
-SNITCH_EXPORT [[noreturn]] void assertion_failed(std::string_view msg);
+[[noreturn]] SNITCH_EXPORT void assertion_failed(std::string_view msg);
 } // namespace snitch
 
 #endif
