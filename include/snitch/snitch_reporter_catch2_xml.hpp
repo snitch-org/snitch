809f4a91070cf9d460eeb056a45310f2e651fb52
Corentin Schreiber <github@corentin.net>
2024-10-11 15:41:22 +0100

    Fix #185

--- include/snitch/snitch_reporter_catch2_xml.hpp
+++ include/snitch/snitch_reporter_catch2_xml.hpp
@@ -3,7 +3,7 @@
 
 #include "snitch/snitch_config.hpp"
 
-#if defined(SNITCH_WITH_CATCH2_XML_REPORTER) || defined(SNITCH_WITH_ALL_REPORTERS)
+#if SNITCH_WITH_CATCH2_XML_REPORTER || SNITCH_WITH_ALL_REPORTERS
 
 #    include "snitch/snitch_test_data.hpp"
 
