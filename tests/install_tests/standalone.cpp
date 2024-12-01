d11ca821bb4219b81a4c6bfcd6a8df3bdd4d127c
Corentin Schreiber <github@corentin.net>
2024-12-01 11:22:30 +0000

    Use snitch.hpp in install test to catch missing exported headers

--- tests/install_tests/standalone.cpp
+++ tests/install_tests/standalone.cpp
@@ -2,8 +2,7 @@
 #    define SNITCH_IMPLEMENTATION
 #    include <snitch/snitch_all.hpp>
 #else
-#    include <snitch/snitch_macros_check.hpp>
-#    include <snitch/snitch_macros_test_case.hpp>
+#    include <snitch/snitch.hpp>
 #endif
 
 TEST_CASE("compiles and runs") {
